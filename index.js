a185a0ba1db5489bfd1f1f3f0f4f2e8602492980
katskurka <kati.skurka@gmail.com>
2021-08-07 14:07:56 -0400

    created initial index.js structure and created salary check function

---
+++ index.js
@@ -0,0 +1,27 @@
+// Lineup Rules
+// The total salary of all players in a lineup may not exceed $45,000
+// Lineups may not contain more than 2 players from a single team
+// Lineups may not contain more than 3 players from a single game
+// Lineups must contain exactly 3 players with the position of 'OF' and must also contain exactly 1 player from each of the following positions: 'P', 'C', '1B', '2B', '3B', 'SS'
+
+const testLineup = ['1B', '2B', '3B', 'C', 'OF', 'OF', 'OF', 'P', 'SS']
+
+// Check Player Team
+
+// Check # of Games
+
+// Check Player Positions
+
+function playerPosition
+
+// Check Team Salary
+
+const checkSalary = (lineup) => {
+  let totalSalary = 0
+
+  lineup.forEach(player => totalSalary += player.salary)
+
+  return (totalSalary <= 45000) ? true : false
+}
+
+module.exports = validateLineup
